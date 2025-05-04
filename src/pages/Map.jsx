a2c8210bd3f65f409832dbc961b8d1799236014d
qkrxogmla <suk05553@naver.com>
2025-05-04 12:51:26 +0900

    지도 이동시 주차장 리렌더링

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -65,7 +65,7 @@ const Map = () => {
     ps.keywordSearch(keyword, placesSearchCB);
   };
 
-  // 검색 결과 마커 렌더링
+  // 검색 결과 장소 마커 렌더링
   useEffect(() => {
     if (!mapRef.current || !places.length) return;
 
@@ -112,7 +112,7 @@ const Map = () => {
     placeMarkerRef.current = newMarkers;
   }, [places]);
 
-  // 주변 주차장 마커 렌더링
+  // 최초 주변 주차장 마커 렌더링
   useEffect(() => {
     if (selectedPlace) {
       fetchNearbyParkingLots(selectedPlace.getLat(), selectedPlace.getLng())
@@ -144,6 +144,56 @@ const Map = () => {
         });
     }
   }, [selectedPlace]);
+
+  // 지도 드래그 후 주차장 마커 렌더링
+  useEffect(() => {
+    if (!mapRef.current) return;
+
+    const map = mapRef.current;
+
+    const handleDragEnd = () => {
+      const center = map.getCenter();
+      const lat = center.getLat();
+      const lng = center.getLng();
+
+      console.log("지도 중심 이동됨:", lat, lng);
+
+      fetchNearbyParkingLots(lat, lng)
+        .then((parkingLots) => {
+          console.log("이동 후 주차장:", parkingLots);
+
+          parkingLotMarkersRef.current.forEach((m) => m.setMap(null));
+          parkingLotMarkersRef.current = [];
+
+          const newMarkers = parkingLots.map((lot) => {
+            const position = new window.kakao.maps.LatLng(
+              lot.latitude,
+              lot.longitude
+            );
+
+            const marker = new window.kakao.maps.Marker({
+              map,
+              position,
+            });
+
+            return marker;
+          });
+
+          parkingLotMarkersRef.current = newMarkers;
+        })
+        .catch((err) => {
+          console.error("지도 이동 시 API 실패:", err);
+        });
+    };
+
+    // 중심 좌표 변경 감지 (드래그, 줌 등)
+    window.kakao.maps.event.addListener(map, "idle", handleDragEnd);
+
+    // 정리 함수 (컴포넌트 언마운트 시)
+    return () => {
+      window.kakao.maps.event.removeListener(map, "idle", handleDragEnd);
+    };
+  }, [loaded]);
 
   return (
     <div className={styles.wrapper}>
